01a2a07c17286f7f30c59242ea80c1e769647d33
Preygrammer <reynzkie16@gmail.com>
2021-08-23 01:22:25 +0800

    Dynamic options via props

---
+++ src/components/AppSelect.vue
@@ -0,0 +1,19 @@
+<template>
+  <select name="select">
+    <option v-for="item in items" :key="item.value" :value="item.value">
+      {{ item.text }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "AppSelect",
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
