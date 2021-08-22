2752ffb8f78be5674810f7f58586507300aab98d
Preygrammer <reynzkie16@gmail.com>
2021-08-23 01:32:34 +0800

    Added a new route (checkout)

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,15 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import ( /* webpackChunkName: "about" */ '../views/Cart.vue')
+    },
+    {
+        path: '/checkout',
+        name: 'Checkout',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Checkout.vue')
     }
 ]
 
