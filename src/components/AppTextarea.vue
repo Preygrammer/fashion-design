7f80a9e6096960ef33830146751c92d5304a2990
Preygrammer <reynzkie16@gmail.com>
2021-08-23 01:24:06 +0800

    Textarea component

---
+++ src/components/AppTextarea.vue
@@ -0,0 +1,12 @@
+<template>
+  <textarea :placeholder="placeholder"></textarea>
+</template>
+
+<script>
+export default {
+  name: "AppTextarea",
+  props: {
+    placeholder: String,
+  },
+};
+</script>
