3207f05623fba31051fe3a1060555eeda81c6699
Preygrammer <reynzkie16@gmail.com>
2021-09-01 17:32:38 +0800

    Rename BorderLine component to "AppBorderLine"

---
+++ src/components/AppBorderLine.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="border-wrapper">
+    <div class="categories__border__line" :style="style"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBorderLine",
+  props: {
+    bgColor: String,
+    width: String,
+    location: String,
+  },
+  computed: {
+    style() {
+      return {
+        backgroundColor: this.bgColor,
+        width: this.width,
+        margin: `10px ${this.location == "center" ? "auto 0" : "0"}`,
+      };
+    },
+  },
+};
+</script>
+
+
+<style>
+.categories__border__line {
+  height: 2px;
+  margin: 10px auto 0;
+}
+</style>
