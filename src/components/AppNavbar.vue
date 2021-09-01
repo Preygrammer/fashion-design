d7c3e88f430f5801c125a64cae39a39e56872b59
Preygrammer <reynzkie16@gmail.com>
2021-09-01 17:35:07 +0800

    Modify AppNavbar component

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar-container">
-    <div class="navbar-top">
+    <div class="navbar-top navbar-desktop">
       <div class="left__side">
         <AppIcon icon="user-alt" />
         <AppIcon icon="heart" />
@@ -16,8 +16,17 @@
         <AppIcon icon="bars" />
       </div>
     </div>
-    <div class="navbar-bottom">
+    <div class="navbar-bottom navbar-desktop">
       <List :hasRouteItems="true" :items="routeItems" />
+    </div>
+
+    <div class="navbar-mobile">
+      <div class="navbar-mobile-logo">
+        <h1>LOGO</h1>
+      </div>
+    </div>
+    <div class="navbar-toggle-menu navbar-mobile">
+      <AppIcon icon="bars" />
     </div>
   </div>
 </template>
@@ -48,110 +57,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.navbar-logo {
-  font-family: "Poppins";
-  /*Temp*/
-}
-
-a {
-  color: #ffffff;
-  font-family: "Oswald";
-  font-weight: 100;
-  font-size: 12px;
-  letter-spacing: 2px;
-}
-
-a:hover {
-  color: #ffffff;
-}
-
-.navbar-container {
-  height: 182px;
-  padding: 0 70px;
-  width: 100%;
-  background: #1e1f1c;
-}
-
-.navbar-top {
-  display: flex;
-  padding-top: 14px;
-}
-
-.left__side,
-.center__side,
-.right__side {
-  width: 33.33%;
-}
-
-.left__side,
-.right__side {
-  color: #ffffff;
-  padding-top: 52px;
-}
-
-.left__side {
-  text-align: right;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-}
-
-.right__side {
-  text-align: right;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-}
-
-.left__side .icon__wrapper,
-.right__side .icon__wrapper {
-  margin: 0 10px;
-}
-
-.right__side .icon__wrapper:nth-child(2) {
-  margin-right: 18px;
-}
-
-.center__side {
-  text-align: center;
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
-}
-
-.center__side {
-  color: #ffffff;
-}
-
-.navbar-logo h1 {
-  font-size: 80px;
-}
-
-.navbar-bottom {
-  font-family: Oswald-Light;
-  text-transform: uppercase;
-  font-size: 12px;
-  width: 100%;
-  display: flex;
-  align-content: center;
-  justify-content: center;
-  align-items: center;
-  height: 58px;
-  margin-top: 5px;
-  border-top: 1px solid #2d2d2d;
-}
-
-.navbar-bottom ul {
-  list-style: none;
-  display: flex;
-  gap: 60px;
-  margin-bottom: 0;
-}
-
-.navbar-bottom ul a {
-  color: #ffffff;
-  text-decoration: none !important;
-}
-</style>
