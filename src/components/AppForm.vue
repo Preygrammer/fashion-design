f7f2eedb35b68cb7228dbe6230aaee38ab75a1ca
Preygrammer <reynzkie16@gmail.com>
2021-09-01 17:32:51 +0800

    Rename Form component to "AppForm"

---
+++ src/components/AppForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <form :method="method" :action="url">
+    <slot />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AppForm",
+  props: {
+    url: String,
+    method: String,
+  },
+};
+</script>
