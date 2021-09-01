286c0de0cbedd2b7dbf41a9a99a76f3330d9146a
Preygrammer <reynzkie16@gmail.com>
2021-09-01 17:49:47 +0800

    Created TheProductCard component

---
+++ src/components/TheProductCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="the__product__card">
+    <!-- <div class="fpc__sale-tag" v-if="isSale">Sale</div> -->
+    <AppImage :imgSrc="imgSrc" />
+
+    <slot />
+  </div>
+</template>
+
+<script>
+import AppImage from "@/components/AppImage.vue";
+
+export default {
+  name: "TheProductCard",
+  components: {
+    AppImage,
+  },
+  props: {
+    imgSrc: String,
+  },
+};
+</script>
