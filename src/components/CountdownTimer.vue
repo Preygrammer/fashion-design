c118227f2ab77ed858a9d98250bab99a1937b283
Preygrammer <reynzkie16@gmail.com>
2021-09-01 17:42:58 +0800

    Added (data, props, and created method) and dynamic/reactive data for countdown

--- src/components/CountdownTimer.vue
+++ src/components/CountdownTimer.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="countdown__timer__content">
     <div class="countdown__timer__details">
-      <div class="ct__number">650</div>
+      <div class="ct__number">{{ cdDays }}</div>
       <div class="ct__text">Days</div>
     </div>
     <div class="countdown__timer__details">
-      <div class="ct__number">21</div>
+      <div class="ct__number">{{ cdHours }}</div>
       <div class="ct__text">Hrs</div>
     </div>
     <div class="countdown__timer__details">
-      <div class="ct__number">50</div>
+      <div class="ct__number">{{ cdMinutes }}</div>
       <div class="ct__text">Mins</div>
     </div>
     <div class="countdown__timer__details">
-      <div class="ct__number">30</div>
+      <div class="ct__number">{{ cdSeconds }}</div>
       <div class="ct__text">Secs</div>
     </div>
   </div>
@@ -22,10 +22,50 @@
 <script>
 export default {
   name: "CountdownTimer",
+
+  data() {
+    return {
+      cdDays: 0,
+      cdHours: 0,
+      cdMinutes: 0,
+      cdSeconds: 0,
+      days: "",
+    };
+  },
+  props: {
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  created: function () {
+    var countdownDate = new Date(this.date).getTime();
+
+    var timeInterval = setInterval(() => {
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countdownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      this.cdDays = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.cdHours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      this.cdMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.cdSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(timeInterval);
+        //do something baby
+      }
+    }, 1000);
+  },
 };
 </script>
 
-<style scoped>
+<style>
 .countdown__timer__content {
   margin-top: 80px;
   margin-bottom: 84px;
